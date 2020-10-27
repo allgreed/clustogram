8a519ac244eb8656168e494c7a3ae57eb8c67140
Labus, Pawel <pawel.labus@falkbuilt.com>
2020-10-27 22:08:30 +0100

    Initial ui setup

---
+++ ui/src/components/GraphView.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Miejsce na Twój {{ appName }}</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    appName: String
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+</style>
