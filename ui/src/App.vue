53f555bfb695a9dffec68c819a9d23cb302358d0
Labus, Pawel <pawel.labus@falkbuilt.com>
2020-11-07 17:17:05 +0100

    Typescript

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,21 +1,27 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <Graphview app-name="Clustogram"/>
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
 
-import GraphView from './components/GraphView.vue'
-
-export default {
+export default defineComponent({
+  name: 'App',
   components: {
-    Graphview: GraphView
+    HelloWorld
   }
-}
+});
 </script>
 
 <style>
 #app {
-  font-family: Arial sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
