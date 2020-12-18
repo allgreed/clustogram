f029e5d18458239834db8dbf560640db7572a719
Olgierd "Allgreed" Kasprowicz <olgierd@kasprowicz.pro>
2020-12-18 21:26:45 +0100

    Make sure UI is up to date

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 #TODO: does not work with multiple files
 DATA_ENTRYPOINT=${1:-'single.example.yaml'}
 
+nix-shell ./ui/default.nix --run "make -C ui build"
 nix-shell ./cli/default.nix --run "make -C cli run $(pwd)/$DATA_ENTRYPOINT" \
 | UI_STATIC_CONTENT=$(pwd)/ui/dist/ nix-shell ./graph/default.nix --run "make -C graph run"
 
